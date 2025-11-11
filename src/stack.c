f6b0c767c04e12a1f0753f0b4b8a3d53a4ce4be1
Carolina Trejos <carotq777@gmail.com>
2025-11-10 18:25:28 -0600

    Agregar requisitos para compilacion

--- src/stack.c
+++ src/stack.c
@@ -1,10 +1,11 @@
 #include "stack.h"
 
+#include <stdlib.h>
 struct stack {
   int *data; //Puntero al array
   size_t size;
   size_t cap;
-}
+};
 
 stack_t * stack_create(size_t initial_cap) {
   stack_t *s = (stack_t *) calloc(1, sizeof(*s));
@@ -27,4 +28,41 @@ size_t stack_size(const stack_t* s){
 		return 0;
 	}	
 	return s->size;
+}
+
+int stack_push(stack_t * s, int value) {
+  if (!s) return -1; //Pointer guards!
+  if (s->size == s->cap) return -1;
+
+  s->data[s->size++] = value;
+  return 0;
+}
+
+void stack_destroy(stack_t* s)
+{
+  if (!s)
+    return;
+}
+
+int stack_pop(stack_t* s, int* out){
+  if (!s || !out)
+    return -1;
+  return 0;
+}
+
+int stack_top(const stack_t* s, int* out){
+  if (!s || !out)
+    return -1;
+  return 0;
+}
+
+bool stack_is_empty(const stack_t* s){
+  if (!s)
+    return false;
+  return true;
+}
+
+void stack_print(const stack_t* s) {
+  if (!s)
+    return;
 }
